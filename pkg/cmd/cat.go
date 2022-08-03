75b31c3b8e07876653efb93cf7a051aeda436f0d
Craig Wilson <craiggwilson@gmail.com>
2022-08-03 15:26:28 -0500

    used custom type for color

--- pkg/cmd/cat.go
+++ pkg/cmd/cat.go
@@ -1,29 +1,21 @@
 package cmd
 
 import (
-	"github.com/charmbracelet/lipgloss"
 	"github.com/craiggwilson/songtool/pkg/songio"
-	"github.com/muesli/termenv"
 )
 
 type CatCmd struct {
 	songCmd
 
-	NoChords bool `name:"no-chords" help:"Hides chords from the output."`
-	JSON     bool `name:"json" xor:"json" help:"Prints the output as JSON."`
-	Color    bool `name:"color" xor:"json" default:"${color}" negatable:"" help:"Indicates whether to use color"`
+	NoChords bool  `name:"no-chords" help:"Hides chords from the output."`
+	JSON     bool  `name:"json" xor:"json" help:"Prints the output as JSON."`
+	Color    color `name:"color" xor:"json" default:"${color}" negatable:"" help:"Indicates whether to use color"`
 }
 
 func (cmd *CatCmd) Run(cfg *Config) error {
 	defer cmd.ensurePath().Close()
 
 	song := cmd.openSong(cfg)
-
-	if !cmd.Color {
-		lipgloss.SetColorProfile(termenv.Ascii)
-	} else if termenv.EnvColorProfile() == termenv.Ascii {
-		lipgloss.SetColorProfile(termenv.ANSI)
-	}
 
 	if cmd.NoChords {
 		song = songio.RemoveChords(song)
---
+++ pkg/cmd/color.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/charmbracelet/lipgloss"
+	"github.com/muesli/termenv"
+)
+
+type color bool
+
+func (c color) AfterApply() error {
+	if !c {
+		lipgloss.SetColorProfile(termenv.Ascii)
+	} else if termenv.EnvColorProfile() == termenv.Ascii {
+		lipgloss.SetColorProfile(termenv.ANSI)
+	}
+
+	return nil
+}
